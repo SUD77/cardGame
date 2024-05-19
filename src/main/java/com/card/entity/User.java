d0fa4ac76046b21e5e9c49866bd786795003b368
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-05-19 12:48:01 +0530

    feat : User entity creation

---
+++ src/main/java/com/card/entity/User.java
@@ -0,0 +1,20 @@
+package com.card.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String userName;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String userRole;
+
+}
